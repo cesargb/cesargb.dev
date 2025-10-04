47ac8ba1f0ac0c2a8c7ced5ca560590ad21ba4f0
Cesar <cesargb@gmail.com>
2025-10-04 17:43:52 +0200

    meta robots

---
+++ app/Http/Middleware/MetaRobotsMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Context;
+use Symfony\Component\HttpFoundation\Response;
+
+class MetaRobotsMiddleware
+{
+
+    public function handle(Request $request, Closure $next, string $defaultRobots = 'noindex, nofollow'): Response
+    {
+        Context::addHidden('meta.robots', $defaultRobots);
+
+        return $next($request);
+    }
+}
