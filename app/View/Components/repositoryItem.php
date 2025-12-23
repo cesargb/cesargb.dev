3c0deb6b55bbbdf0fbbc523dda742bb27ca0afed
Cesar <cesargb@gmail.com>
2025-12-23 11:35:36 +0100

    clean: clean phpdocs

--- app/View/Components/repositoryItem.php
+++ app/View/Components/repositoryItem.php
@@ -8,17 +8,6 @@ use Illuminate\View\Component;
 
 class repositoryItem extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct(public array $repository)
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.repository-item');
