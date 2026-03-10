1d2175483c7a1dbde182e6de4e4789ea3685e7ca
Cesar <cesargb@gmail.com>
2026-03-10 12:40:18 +0100

    Remove package name from package-lock.json and add Vite::fake() to TestCase setUp

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Vite;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Vite::fake();
+    }
 }
