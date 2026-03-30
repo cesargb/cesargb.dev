d29fefed2936224e714ff36b8893c82d185f4c8c
Cesar <cesargb@gmail.com>
2026-03-30 21:46:32 +0200

    -x

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,26 +1,25 @@
-@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
+@php
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+    $locales = ['es', 'en'];
+    $paths = [
+        'index',
+        'lab.js-api-translate',
+    ];
+@endphp
 <urlset
     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xhtml="http://www.w3.org/1999/xhtml"
 >
+@foreach ($paths as $path)
+@foreach ($locales as $locale)
+@php $routeName = "{$locale}.{$path}"; @endphp
     <url>
-        <loc>{{ route('es.index') }}</loc>
-        <xhtml:link rel="alternate" hreflang="es" href="{{ route('es.index') }}" />
-        <xhtml:link rel="alternate" hreflang="en" href="{{ route('en.index') }}" />
+        <loc>{{ route($routeName) }}</loc>
+@foreach ($locales as $altLocale)
+        <xhtml:link rel="alternate" hreflang="{{ $altLocale }}" href="{{ route("{$altLocale}.{$path}") }}"/>
+@endforeach
     </url>
-    <url>
-        <loc>{{ route('en.index') }}</loc>
-        <xhtml:link rel="alternate" hreflang="en" href="{{ route('en.index') }}" />
-        <xhtml:link rel="alternate" hreflang="es" href="{{ route('es.index') }}" />
-    </url>
-    <url>
-        <loc>{{ route('es.lab.js-api-translate') }}</loc>
-        <xhtml:link rel="alternate" hreflang="es" href="{{ route('es.lab.js-api-translate') }}" />
-        <xhtml:link rel="alternate" hreflang="en" href="{{ route('en.lab.js-api-translate') }}" />
-    </url>
-    <url>
-        <loc>{{ route('en.lab.js-api-translate') }}</loc>
-        <xhtml:link rel="alternate" hreflang="en" href="{{ route('en.lab.js-api-translate') }}" />
-        <xhtml:link rel="alternate" hreflang="es" href="{{ route('es.lab.js-api-translate') }}" />
-    </url>
+@endforeach
+@endforeach
 </urlset>
