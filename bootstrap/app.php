9df1d536fee85598ecb2105bc7f6b19be0244ebe
Cesar <cesargb@gmail.com>
2026-06-17 16:08:22 +0200

    Refactor User-related references to use imported class names instead of fully qualified names

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AppMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(App\Http\Middleware\AppMiddleware::class);
+        $middleware->append(AppMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
